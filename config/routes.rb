88510f3e8d2c36ce8eb5e010156d6083aadb3a9d
Tomoki Harada <tellyourworlds@gmail.com>
2017-03-22 12:51:26 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
@@ -10,6 +8,11 @@ Rails.application.routes.draw do
   get '/login',   to: 'sessions#new'
   post '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only:[:create, :destroy]
 end
